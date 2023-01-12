2b082ecd08f5fe8511d1ea3b37835705ef85ede2
Xalqa-Cintra <113114207+Xalqa-Cintra@users.noreply.github.com>
2023-01-12 15:20:17 +0000

    mission screen base finished

--- Assets/Code/MissionScreen.cs
+++ Assets/Code/MissionScreen.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using UnityEngine.XR;
 
 
 public class MissionScreen : MonoBehaviour
 {
     [Header("Menu")]
+    public GameObject openPhone;
     public GameObject missionUI;
     public GameObject note1;
     public GameObject note2;
@@ -19,72 +21,89 @@ public class MissionScreen : MonoBehaviour
 
     [Header("Errand Tab")]
     public GameObject errandUI;
-    public GameObject errand1text;
-    public GameObject errand2text;
-    public GameObject errand3text;
-    public GameObject errand4text;
-    public GameObject errand5text;
+    public GameObject errandTextBox;
+
+    [Header ("Text")]
+    public string errand1Text = ("Go help the bullfighter");
+    public string errand2Text = ("Go help the Flamenco dancer");
+    public string errand3Text = ("Go fix the bridge");
+    public string errand4Text = ("Go help the elderly");
+    public string errand5Text = ("Go save Mr. Mariachi");
+
+    Text textElement;
 
     private void Start() 
     {
         missionUI.SetActive(false);
         errandUI.SetActive(false);
+        returnButton.SetActive(false);
+        textElement = errandTextBox.GetComponent<Text>();
     }
     
 
     public void OpenMenu()
     {
+        openPhone.SetActive(false);
         missionUI.SetActive(true);
+
 
     }
     
     public void CloseMenu()
     {
         missionUI.SetActive(false);
-
+        openPhone.SetActive(true);
         errandUI.SetActive(false);
     }
 
     public void OpenNote()
     {
         missionUI.SetActive(false);
-
         errandUI.SetActive(true);
+        returnButton.SetActive(true);
+        errandTextBox.SetActive(true);
     }
     public void OpenNote1() 
     {
 
-        errand1text.SetActive(true);
+        textElement.text = errand1Text;
         //if(missioncheckbool=true){settexttothis}
 
     }
     public void OpenNote2() 
     {
 
-        errand2text.SetActive(true);
+        textElement.text = errand2Text;
         //if(missioncheckbool=true){settexttothis}
 
     }
     public void OpenNote3() 
     {
 
-        errand3text.SetActive(true);
+        textElement.text = errand3Text;
         //if(missioncheckbool=true){settexttothis}
 
     }
     public void OpenNote4() 
     {
 
-        errand4text.SetActive(true);
+        textElement.text = errand4Text;
         //if(missioncheckbool=true){settexttothis}
 
     }
     public void OpenNote5() 
     {
 
-        errand5text.SetActive(true);
+        textElement.text = errand5Text;
         //if(missioncheckbool=true){settexttothis}
 
+    }
+
+    public void returnScreen()
+    {
+        errandUI.SetActive(false);
+        missionUI.SetActive(true);
+        errandTextBox.SetActive(false);
     }
 
 }
